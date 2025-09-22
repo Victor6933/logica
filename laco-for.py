faea397119820d490d2f305050605d2de177b307
Victor6933 <cordeiro.lima.victor@escola.pr.gov.br>
2025-09-22 12:18:54 -0300

    Update laco-for.py

--- laco-for.py
+++ laco-for.py
@@ -1,1 +1,7 @@
+https://www.programiz.com/online-compiler/38MSLpi2fDBKv
+https://www.programiz.com/online-compiler/4etLHpmvV81lk
+https://www.programiz.com/online-compiler/2Cgyfgqm3pKlU
+https://www.programiz.com/online-compiler/89J8e4rl7i2C3
+https://www.programiz.com/online-compiler/7xlKOBEeVrnzA
+https://www.programiz.com/online-compiler/7ys9Fs1eGdLol
 https://www.programiz.com/online-compiler/5jCgcO6XJvOCX
